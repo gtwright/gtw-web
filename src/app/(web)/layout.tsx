93288f334ed4becd8eb42b8aa2f2554c52445336
Graham <gwright@bso.org>
2024-12-18 19:14:23 -0500

    chore: update dependencies and add Tailwind CSS configuration
    - Added Tailwind CSS and PostCSS configuration files.
    - Updated package.json and pnpm-lock.yaml to include new dependencies: Tailwind CSS, Autoprefixer, and PostCSS.
    - Enhanced layout and page components to utilize Google Fonts and Tailwind CSS for styling.
    - Created global CSS file for Tailwind directives.

--- src/app/(web)/layout.tsx
+++ src/app/(web)/layout.tsx
@@ -1,9 +1,35 @@
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+import type { Metadata } from 'next'
+
+// These styles apply to every route in the application
+import '@/styles/globals.css'
+import { Noto_Serif, Noto_Sans } from 'next/font/google'
+
+const notoSerif = Noto_Serif({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  display: 'swap',
+  variable: '--font-noto-serif',
+})
+
+const notoSans = Noto_Sans({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  display: 'swap',
+  variable: '--font-noto-sans',
+})
+export const metadata: Metadata = {
+  title: 'GTW Web',
+  description: "Graham Wright's website",
+}
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>
-        <main>{children}</main>
-      </body>
+    <html
+      lang="en"
+      className={`${notoSans.variable} ${notoSerif.variable}`}
+      suppressHydrationWarning
+    >
+      <body className="min-h-screen antialiased">{children}</body>
     </html>
   )
 }
