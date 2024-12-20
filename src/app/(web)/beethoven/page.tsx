5de429fc8bea4459555453c81cfd6b6afe7afb54
Graham <gwright@bso.org>
2024-12-20 13:14:36 -0500

    feat: update Beethoven page to handle search parameters
    - Modified the Page component to accept search parameters, allowing for dynamic fetching of performance data based on the conductor's name.
    - Enhanced the data fetching logic to improve user experience by tailoring content based on input parameters.

--- src/app/(web)/beethoven/page.tsx
+++ src/app/(web)/beethoven/page.tsx
@@ -1,6 +1,10 @@
 import { fetchPerformances } from "@/actions/fetch"
-export default async function Page() {
-  const data = await fetchPerformances("all");
+export default async function Page(props: {
+  searchParams?: Promise<{ conductor?: string }>;
+}) {
+  const searchParams = await props.searchParams;
+  const conductor = searchParams?.conductor || "";
+  const data = await fetchPerformances(conductor);
   console.log(data);
   return (
     <div className="flex flex-col items-center justify-center h-screen">
