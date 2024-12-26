91defcf8f0ee3ebc505556fed2f3cbef8c7373d7
Graham <gwright@bso.org>
2024-12-26 15:51:09 -0500

    feat: enhance Tailwind CSS configuration and global styles
    - Updated tailwind.config.ts to include new typography and animation plugins for improved styling.
    - Added a safelist for responsive column spans and various background/border utilities.
    - Extended theme configuration with new color definitions and keyframes for accordion animations.
    - Refactored global styles in globals.css to streamline heading styles and improve typography consistency.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,24 @@
 import type { Config } from 'tailwindcss'
+import animate from 'tailwindcss-animate'
+import typography from '@tailwindcss/typography'
 
 export default {
   darkMode: ['class'],
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  safelist: [
+    'lg:col-span-4',
+    'lg:col-span-6',
+    'lg:col-span-8',
+    'lg:col-span-12',
+    'border-border',
+    'bg-card',
+    'border-error',
+    'bg-error/30',
+    'border-success',
+    'bg-success/30',
+    'border-warning',
+    'bg-warning/30',
+  ],
   theme: {
     container: {
       center: true,
@@ -14,20 +30,118 @@ export default {
         sm: '1rem',
         xl: '2rem',
       },
+      screens: {
+        '2xl': '86rem',
+        lg: '64rem',
+        md: '48rem',
+        sm: '40rem',
+        xl: '80rem',
+      },
     },
     extend: {
-      fontFamily: {
-        sans: ['var(--font-noto-sans)'],
-        serif: ['var(--font-noto-serif)'],
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
       },
       borderRadius: {
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)',
       },
-      colors: {},
+      colors: {
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
+        },
+        background: 'hsl(var(--background))',
+        border: 'hsla(var(--border))',
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
+        },
+        foreground: 'hsl(var(--foreground))',
+        input: 'hsl(var(--input))',
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
+        },
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        ring: 'hsl(var(--ring))',
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
+        success: 'hsl(var(--success))',
+        error: 'hsl(var(--error))',
+        warning: 'hsl(var(--warning))',
+      },
+      fontFamily: {
+        // mono: ['var(--font-geist-mono)'],
+        sans: ['var(--font-noto-sans)'],
+        serif: ['var(--font-noto-serif)'],
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      typography: () => ({
+        DEFAULT: {
+          css: [
+            {
+              '--tw-prose-body': 'var(--text)',
+              '--tw-prose-headings': 'var(--text)',
+              h1: {
+                fontWeight: 600,
+                marginBottom: '0.25em',
+              },
+            },
+          ],
+        },
+        base: {
+          css: [
+            {
+              h1: {
+                fontSize: '2.5rem',
+                fontWeight: 600,
+              },
+              h2: {
+                fontSize: '1.25rem',
+                fontWeight: 600,
+              },
+            },
+          ],
+        },
+        md: {
+          css: [
+            {
+              h1: {
+                fontSize: '3.5rem',
+              },
+              h2: {
+                fontSize: '1.5rem',
+              },
+            },
+          ],
+        },
+      }),
     },
   },
-  // @ts-expect-error - Tailwind plugin import
-  plugins: [require('tailwindcss-animate'), require('tailwindcss-typography')],
+  plugins: [animate, typography],
 } satisfies Config
