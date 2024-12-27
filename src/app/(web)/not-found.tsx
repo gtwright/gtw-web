34fe8f3ea8ad41ac03c1f9c9dcce2a1a650675e2
Graham <gwright@bso.org>
2024-12-27 16:54:22 -0500

    feat: track 'not found' page visits with PostHog integration
    - Added PostHog tracking to the NotFound component to capture user interactions when a 404 page is displayed.
    - Implemented useEffect to trigger the 'not_found' event upon component mount, enhancing analytics capabilities.

--- src/app/(web)/not-found.tsx
+++ src/app/(web)/not-found.tsx
@@ -1,9 +1,18 @@
+'use client'
+
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import { Button } from '@/components/ui/button'
+import { usePostHog } from 'posthog-js/react'
 
 export default function NotFound() {
+  const posthog = usePostHog()
+
+  useEffect(() => {
+    posthog.capture('not_found')
+  }, [])
+
   return (
     <div className="container py-28">
       <div className="prose max-w-none">
