56159deebd1c94016568ae68738f74a8bfc6ef48
Graham <gwright@bso.org>
2024-12-27 20:46:54 -0500

    feat: integrate S3 storage support in payload configuration
    - Added @payloadcms/storage-s3 dependency to enable S3 storage capabilities.
    - Updated package.json and pnpm-lock.yaml to include the new storage dependency.
    - Configured S3 storage in payload.config.ts with bucket details and credentials for media collection management.
    - Enhanced Media interface in payload-types.ts to include an optional prefix property for better media organization.

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -102,6 +102,7 @@ export interface Post {
 export interface Media {
   id: number;
   alt: string;
+  prefix?: string | null;
   updatedAt: string;
   createdAt: string;
   url?: string | null;
@@ -228,6 +229,7 @@ export interface PostsSelect<T extends boolean = true> {
  */
 export interface MediaSelect<T extends boolean = true> {
   alt?: T;
+  prefix?: T;
   updatedAt?: T;
   createdAt?: T;
   url?: T;
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,6 +1,5 @@
 // storage-adapter-import-placeholder
 import { vercelPostgresAdapter } from '@payloadcms/db-vercel-postgres'
-import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import path from 'path'
 import { buildConfig } from 'payload'
@@ -15,6 +14,7 @@ import { Header } from './globals/Header'
 import { Footer } from './globals/Footer'
 import { getServerSideURL } from './lib/utils/getURL'
 import { resendAdapter } from '@payloadcms/email-resend'
+import { s3Storage } from '@payloadcms/storage-s3'
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
@@ -53,7 +53,25 @@ export default buildConfig({
     outputFile: path.resolve(dirname, 'payload-types.ts'),
   },
   plugins: [
-    payloadCloudPlugin(),
+    // payloadCloudPlugin(),
     // storage-adapter-placeholder
+    s3Storage({
+      collections: {
+        media: {
+          prefix: 'media',
+        },
+      },
+      bucket: 'gtw-dev-1',
+
+      config: {
+        forcePathStyle: true,
+        credentials: {
+          accessKeyId: process.env.S3_ACCESS_KEY_ID || '',
+          secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || '',
+        },
+        region: process.env.S3_REGION || '',
+        endpoint: process.env.S3_ENDPOINT || '',
+      },
+    }),
   ],
 })
