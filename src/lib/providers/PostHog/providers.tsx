5bd09e9ff26890c35253a4bf81549bb6e5e1ba91
Graham <gtwright@gmail.com>
2025-06-15 17:02:08 -0400

    feat: if posthog env is missing do not init

--- src/lib/providers/PostHog/providers.tsx
+++ src/lib/providers/PostHog/providers.tsx
@@ -6,14 +6,22 @@ import { PostHogProvider as PHProvider } from 'posthog-js/react'
 import { useEffect } from 'react'
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
+  const key = process.env.NEXT_PUBLIC_POSTHOG_KEY
+
   useEffect(() => {
-    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-      capture_pageview: false, // Disable automatic pageview capture, as we capture manually
-      capture_pageleave: false,
-      api_host: '/ingest',
-      ui_host: 'https://us.posthog.com',
-    })
-  }, [])
+    if (key) {
+      posthog.init(key, {
+        capture_pageview: false, // Disable automatic pageview capture, as we capture manually
+        capture_pageleave: false,
+        api_host: '/ingest',
+        ui_host: 'https://us.posthog.com',
+      })
+    }
+  }, [key])
+
+  if (!key) {
+    return <>{children}</>
+  }
 
   return <PHProvider client={posthog}>{children}</PHProvider>
 }
